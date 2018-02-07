c86fbe8e815fa8b512a90934156903d68c5189c6
nstehr <nstehr@gmail.com>
2018-02-06 19:30:29 -0500

    minor refactor

--- player/player.go
+++ player/player.go
@@ -13,11 +13,15 @@ type Player interface {
 	Play(session *rtsp.Session)
 }
 
+// LocalPlayer is a player that will just play the audio locally
 type LocalPlayer struct{}
 
+// NewLocalPlayer instantiates a new LocalPlayer
 func NewLocalPlayer() *LocalPlayer {
 	return &LocalPlayer{}
 }
+
+// Play will play the packets received on the specified session
 func (*LocalPlayer) Play(session *rtsp.Session) {
 	go func() {
 		p, err := oto.NewPlayer(44100, 2, 2, 10000)
@@ -25,18 +29,19 @@ func (*LocalPlayer) Play(session *rtsp.Session) {
 			log.Println("error initializing player", err)
 			return
 		}
+		var decoder codecHandler
+		rtpmap := session.Description.Attributes["rtpmap"]
+		if strings.Contains(rtpmap, "AppleLossless") {
+			decoder = codecMap["AppleLossless"]
+		} else {
+			decoder = func(data []byte) ([]byte, error) { return data, nil }
+		}
 		for d := range session.OutputChan {
-			rtpmap := session.Description.Attributes["rtpmap"]
-			if strings.Contains(rtpmap, "AppleLossless") {
-				decoded, err := codecMap["AppleLossless"](d)
-				if err != nil {
-					log.Println("Problem decoding packet")
-				}
-				p.Write(decoded)
-			} else {
-				p.Write(d)
+			decoded, err := decoder(d)
+			if err != nil {
+				log.Println("Problem decoding packet")
 			}
-
+			p.Write(decoded)
 		}
 	}()
 
