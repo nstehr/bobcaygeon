b7d73dbe439fa42c21715c71085f24e006358d83
nstehr <nstehr@gmail.com>
2018-03-13 22:12:15 -0400

    add verbose

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-  $GOPATH/bin/goveralls -service=travis-ci
+  $GOPATH/bin/goveralls -v -service=travis-ci
 fi
 
 go build -o bcg-$TRAVIS_OS_NAME cmd/bcg.go
