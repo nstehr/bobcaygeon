84b3ca36202a9f4391804462b1ff0e1a2223641d
nstehr <nstehr@gmail.com>
2018-03-13 21:41:39 -0400

    more build tweaks

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    echo 'osx!'
-    go build -o bcg-osx cmd/bcg.go
-else
-    go build -o bcg-linux cmd/bcg.go
-fi
-ls
+go build -o bcg-$TRAVIS_OS_NAME cmd/bcg.go
