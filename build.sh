828f102d37c919c384486d60453db010e8c3b370
nstehr <nstehr@gmail.com>
2018-03-13 21:55:32 -0400

    more coverage

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-$GOPATH/bin/goveralls -service=travis-ci
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+  $GOPATH/bin/goveralls -service=travis-ci
+fi
+
 go build -o bcg-$TRAVIS_OS_NAME cmd/bcg.go
