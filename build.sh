6d1e1d34ea46d79b9e08ed1a29ac1cd05b4be704
nstehr <nstehr@gmail.com>
2018-03-15 20:27:58 -0400

    more coverage

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ do
 done
 if [[ $TRAVIS_OS_NAME == 'osx' ]]
 then
-   $GOPATH/bin/goveralls -v -service=travis-ci
+   $GOPATH/bin/goveralls -coverprofile=acc.out -service=travis-ci
 fi  
 
 go build -o bcg-$TRAVIS_OS_NAME cmd/bcg.go
