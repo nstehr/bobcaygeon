f0ee404d3e54cd281313c95a9bee0b8411503b7a
Nathan Stehr <nstehr@gmail.com>
2019-03-16 15:13:08 -0400

    Move to Go Modules (#7)
    Moves to go modules and go 1.12. Ditching vendoring for now. Things seem to be all working. Needed a workaround for the arm build, I pull the dependencies into vendor and then the arm build will use the vendor to build, when building on arm I kept getting panics when it was resolving over the network so this my workaround for now.

--- init-arm-build.sh
+++ init-arm-build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# hack for now
+go mod vendor
+
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 docker run --rm -v "$PWD":/usr/gopath/src/github.com/nstehr/bobcaygeon -w /usr/gopath/src/github.com/nstehr/bobcaygeon balenalib/raspberry-pi-golang ./build-arm.sh
