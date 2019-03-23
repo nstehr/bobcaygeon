29db36e6b66e66fb4eeb3f61118813edb140514b
nstehr <nstehr@gmail.com>
2019-03-23 17:15:16 -0400

    updating gitignre; making proto script more useable

--- build_protos.sh
+++ build_protos.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export GOBIN=$PWD/bin
+export PATH=$GOBIN:$PATH
+go install github.com/golang/protobuf/protoc-gen-go
+
 protoc -I api/ --go_out=plugins=grpc:api api/bobcaygeon.proto
 protoc -I cmd/mgmt/api --go_out=plugins=grpc:cmd/mgmt/api cmd/mgmt/api/management.proto
 protoc -I=cmd/mgmt/api cmd/mgmt/api/management.proto --js_out=import_style=commonjs:cmd/frontend/webui/src/api
